2c4982aaa7206d715024d88b6214d2326874620b
Ryan <ryangcase@gmail.com>
2017-01-19 08:44:53 -0500

    Add ReactiveKafkaFromHttpScala

--- build.sbt
+++ build.sbt
@@ -28,10 +28,12 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-testkit" % "2.4.16" % "test",
   "com.typesafe.akka" %% "akka-stream-kafka" % "0.13",
 
-  //"com.typesafe.akka" %% "akka-http-core" % "2.4.11",
-  //"com.typesafe.akka" %% "akka-http-experimental" % "2.4.11",
-  //"com.typesafe.akka" %% "akka-http-spray-json-experimental" % "2.4.11",
-  //"com.typesafe.akka" %% "akka-http-testkit" % "2.4.11",
+  "com.typesafe.akka" %% "akka-http-core" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http-jackson" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http-xml" % "10.0.1",
 
   "org.scalatest" %% "scalatest" % "3.0.0" % "test",
   "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
---
+++ src/main/scala/org/rgcase/reactivekafka/ReactiveKafkaFromHttpScala.scala
@@ -0,0 +1,64 @@
+package org.rgcase.reactivekafka
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.server.Directives._
+import akka.kafka.ProducerMessage.Message
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.ByteArraySerializer
+
+import scala.concurrent.Future
+
+class ReactiveKafkaFromHttpScala {
+
+  implicit val system = ActorSystem("fromhttp")
+  implicit val mat = ActorMaterializer()
+
+  val serverSource = Http().bind(interface = "localhost", port = 8080)
+
+  val producerSettings = ProducerSettings(system, new ByteArraySerializer, new ByteArraySerializer)
+    .withBootstrapServers("localhost:9092")
+
+  def toProducerMessage(topic: String)(chunk: Array[Byte]) = {
+    Message[Array[Byte], Array[Byte], NotUsed](new ProducerRecord(topic, chunk), NotUsed)
+  }
+
+  val handler: HttpRequest => Future[HttpResponse] = {
+
+    // Chunked payload
+    case HttpRequest(POST, Uri.Path("/chunks"), _, entity, _) =>
+      entity
+        .dataBytes
+        .map(chunk => toProducerMessage("targetchunktopic")(chunk.toArray))
+        .via(Producer.flow(producerSettings))
+        .runWith(Sink.ignore)
+        .map(_ => HttpResponse(StatusCodes.Created))
+
+    // Full payload
+    case HttpRequest(POST, Uri.Path("/"), _, entity, _) =>
+      val fut =
+        entity
+          .dataBytes
+          .runWith(Sink.fold(Array.empty[Byte]){ case (acc, bytes) => acc ++ bytes })
+
+      Source
+        .fromFuture(fut)
+        .map(bytes => toProducerMessage("targetfulltopic")(bytes))
+        .via(Producer.flow(producerSettings))
+        .runWith(Sink.ignore)
+        .map(_ => HttpResponse(StatusCodes.Created))
+
+  }
+
+  serverSource.to(Sink.foreach { connection =>
+    connection.handleWithAsyncHandler(handler)
+  }).run()
+
+}
